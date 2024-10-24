74b6beb315f30f725279631a0f22878072ca5269
eswar-03 <panyameswarguru@gmail.com>
2024-10-24 21:49:31 +0530

    pride project complected

---
+++ Events_publisher-update/events2.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Bootstrap Web Design</title>
+        <?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+        <?php require 'utils/scripts.php'; ?><!--js links. file found in utils folder-->
+    </head>
+    <body>
+        <?php require 'utils/header.php'; ?><!--header content. file found in utils folder-->
+        <div class="content"><!--body content holder-->
+            <div class="container">
+                <div class="col-md-12"><!--body content title holder with 12 grid columns-->
+                    <h1>What's On</h1><!--body content title-->
+                </div>
+            </div>
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+			
+            <div class="row"><!--event content-->
+                <section>
+                    <div class="container">
+                        <div class="date col-md-1"><!--date holder with 1 grid column-->
+                            <span class="month">JAN</span><br><!--month-->
+                            <hr class="line"><!--css modified horizontal line-->
+                            <span class="day">20</span><!--day-->
+                        </div>
+                        <div class="col-md-5"><!--image holder with 5 grid column-->
+                            <img src="images/bdayevent.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6"><!--event content holder with 6 grid column-->
+                            <h1 class="title">Dinning Hall</h1><!--event content title-->
+                            <p class="location"><!--event content location-->
+                            UrbanXchange Private Dining Room, The Rocks 12 Argyle Street
+                            </p>
+                            <p class="definition"><!--event content definition-->
+                            The dining hall is a spacious, vibrant area designed to provide a welcoming environment for students, faculty, and guests. Large windows allow natural light to flood the room during the day, creating a lively and comfortable atmosphere. The hall features a variety of seating options, from cozy booths to long communal tables, encouraging both intimate conversations and group gatherings.
+                            </p>
+                            <hr class="customline2"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-lg"><!--view details button (no function implemented)-->
+                            View Details  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><!--arrow right glyphicon-->
+                            </button>
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="date col-md-1"><!--date holder with 1 grid column-->
+                            <span class="month">APR</span><br><!--month-->
+                            <hr class="line"><!--css modified horizontal line-->
+                            <span class="day">20</span><!--day-->
+                        </div>
+                        <div class="col-md-5"><!--image holder with 5 grid column-->
+                            <img src="images/fashevent.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6"><!--event content holder with 6 grid column-->
+                            <h1 class="title">Dress to Impress</h1><!--event content title-->
+                            <p class="location"><!--event content location-->
+                            Ananas Bar & Brasserie, The Rocks 18 Argyle Street
+                            </p>
+                            <p class="definition"><!--event content definition-->
+                            Join us for an exciting event where fashion meets confidence! Dress to Impress is all about expressing yourself through style and making a statement with your presence. Whether it’s formal elegance, smart casuals, or a touch of creativity, this event encourages participants to put their best foot forward and stand out..
+                            </p>
+                            <hr class="customline2"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-lg"><!--view details button (no function implemented)-->
+                            View Details  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><!--arrow right glyphicon-->
+                            </button>
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="date col-md-1"><!--date holder with 1 grid column-->
+                            <span class="month">JUN</span><br><!--month-->
+                            <hr class="line"><!--css modified horizontal line-->
+                            <span class="day">20</span><!--day-->
+                        </div>
+                        <div class="col-md-5"><!--image holder with 5 grid column-->
+                            <img src="images/wedevent.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6"><!--event content holder with 6 grid column-->
+                            <h1 class="title">Our 2nd Anniversary</h1><!--event content title-->
+                            <p class="location"><!--event content location-->
+                            Munich Brauhaus South Wharf, 45 South Wharf Promenade
+                            </p>
+                            <p class="definition"><!--event content definition-->
+                            Join us as we mark the 2nd Anniversary of our incredible journey! This special occasion is a tribute to the collective efforts, achievements, and memories we’ve created along the way. Over the past two years, we’ve grown, evolved, and reached new heights, thanks to the unwavering support of our community, partners, and team.
+                            </p>
+                            <hr class="customline2"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-lg"><!--view details button (no function implemented)-->
+                            View Details  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><!--arrow right glyphicon-->
+                            </button>
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="date col-md-1"><!--date holder with 1 grid column-->
+                            <span class="month">AUG</span><br><!--month-->
+                            <hr class="line"><!--css modified horizontal line-->
+                            <span class="day">20</span><!--day-->
+                        </div>
+                        <div class="col-md-5"><!--image holder with 5 grid column-->
+                            <img src="images/meetevent.jpg" class = "img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6"><!--event content holder with 6 grid column-->
+                            <h1 class="title">Career Talk</h1><!--event content title-->
+                            <p class="location"><!--event content location-->
+                            UrbanXchange Private Dining Room, The Rocks 12 Argyle Street
+                            </p>
+                            <p class="definition"><!--event content definition-->
+                            Career Talk is an engaging and insightful session aimed at guiding students on various career paths and opportunities. The event brings together industry professionals, alumni, and career experts to share real-world experiences, trends, and valuable advice. Through inspiring talks, interactive discussions, and Q&A sessions, participants gain clarity on emerging fields, required skills, and strategies for building a successful career.
+                            </p>
+                            <hr class="customline2"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-lg"><!--view details button (no function implemented)-->
+                                View Details  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><!--arrow right glyphicon-->
+                            </button>
+                       </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+        </div><!--body content div-->
+        <?php require 'utils/footer.php'; ?><!--footer content. file found in utils folder-->
+    </body>
+</html>
---
+++ Events_publisher-update/utils/footer.php
@@ -0,0 +1,37 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>I</strong><span class = "small footerSubtext">nstitute of</span>
+                        <strong>A</strong><span class = "small footerSubtext">rt</span>
+                        <strong>D</strong><span class = "small footerSubtext">esign and</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                    Marina Avenue, Chennai, Tamil Nadu
+                        &copy; Harry Events venue , 2024Venue  , 2024.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    DL824 Multimedia Events publisher/Web Engineering<br>
+                    Creative Computing 2024.<br>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
